a8e9cab15b93567621136d460e1ddd0738b9f09e
JESUS_DANIEL_FLORES_RODRIGUEZ <a.22308051281186@cbtis128.edu.mx>
2025-03-04 16:35:38 +0000

    Autonoma

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,72 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        backgroundColor: Color(0xff111111),
+        elevation: 0,
+        title: Text(
+          "Jesús Daniel Flores Rodríguez", // Texto en el AppBar
+          style: TextStyle(
+            color: Color(0xffcfcfcf), // Color del texto
+            fontSize: 22, // Tamaño del texto
+            fontWeight: FontWeight.bold, // Negrita
+          ),
         ),
+      ),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                color: Colors.blue,
+                height: MediaQuery.of(context).size.height * 0.55,
+                width: double.infinity,
+              ),
+              Expanded(
+                child: Container(
+                  color: Color(0xffd92f2f),
+                  width: double.infinity,
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.47,
+            left: MediaQuery.of(context).size.width * 0.1,
+            child: Container(
+              height: 150,
+              width: MediaQuery.of(context).size.width * 0.8,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0x6d000000),
+                    blurRadius: 5,
+                    spreadRadius: 2,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
